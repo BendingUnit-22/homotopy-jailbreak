984b6e0dd89848ba46099ddd21beca440c624415
BendingUnit-22 <bender.rodrguez@yahoo.com>
2024-06-26 15:05:35 -0500

    remove scripts

--- scripts/run_hgcg_zephyr.sh
+++
@@ -1,6 +0,0 @@
-DEVICE=0
-CUDA_VISIBLE_DEVICES=${DEVICE} python gcg_zephyr_dynamic_budget.py --config ../configs/zephyr/zephyr_ckpt_600_cold_start.json
-CUDA_VISIBLE_DEVICES=${DEVICE} python gcg_zephyr_dynamic_budget.py --config ../configs/zephyr/zephyr_ckpt_200_warm_start.json
-CUDA_VISIBLE_DEVICES=${DEVICE} python gcg_zephyr_dynamic_budget.py --config ../configs/zephyr/zephyr_ckpt_100_warm_start.json
-CUDA_VISIBLE_DEVICES=${DEVICE} python gcg_zephyr_dynamic_budget.py --config ../configs/zephyr/zephyr_ckpt_40_warm_start.json
-CUDA_VISIBLE_DEVICES=${DEVICE} python gcg_zephyr_dynamic_budget.py --config ../configs/zephyr/zephyr_base_warm_start.json
